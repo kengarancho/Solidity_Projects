a8376bb0cac305cde8e914ee9efadbc9604ea818
kengarancho <78880653+kengarancho@users.noreply.github.com>
2024-11-28 17:36:42 +0800

    Update Create-Profile.sol
    added limitation for 1 profile per account

--- contracts/Create-Profile.sol
+++ contracts/Create-Profile.sol
@@ -14,8 +14,10 @@ contract CreateProfile {
     mapping (address => Profile) profiles;
 
     function addProfile(string memory _displayName, string memory _bio) public {
+        require(profiles[msg.sender].length == 0, "You already have a profile!");
         require(bytes(_displayName).length <= MAX_NAME_LENGTH, "Display Name too long!");
         require(bytes(_bio).length <= MAX_BIO_LENGTH, "Bio too long!");
+
         profiles[msg.sender] = Profile(_displayName, _bio);
     }
 
